b0a43f69565ff09530c345e42cb0afc3b20b0a18
hyewonkim <hyewonds.kim@samsung.com>
2025-08-01 13:18:26 +0900

    Step10

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -29,6 +29,14 @@ def booking_scheduler_with_sms_mock():
     return booking_scheduler, testable_sms_sender
 
 
+@pytest.fixture
+def booking_scheduler_with_mail_mock():
+    booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
+    testable_mail_sender = TestableMailSender()
+    booking_scheduler.set_mail_sender(testable_mail_sender)
+    return booking_scheduler, testable_mail_sender
+
+
 def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가(booking_scheduler):
     schedule = Schedule(NOT_ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
 
@@ -73,24 +81,22 @@ def test_예약완료시_SMS는_무조건_발송(booking_scheduler_with_sms_mock
     assert sms_mock.send_called
 
 
-def test_이메일이_없는_경우에는_이메일_미발송(booking_scheduler):
-    mail_sender = TestableMailSender()
-    booking_scheduler.set_mail_sender(mail_sender)
+def test_이메일이_없는_경우에는_이메일_미발송(booking_scheduler_with_mail_mock):
+    booking_scheduler, mail_mock = booking_scheduler_with_mail_mock
     schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
 
     booking_scheduler.add_schedule(schedule)
 
-    assert mail_sender.send_mail_count == 0
+    assert mail_mock.send_mail_count == 0
 
 
-def test_이메일이_있는_경우에는_이메일_발송(booking_scheduler):
-    mail_sender = TestableMailSender()
-    booking_scheduler.set_mail_sender(mail_sender)
+def test_이메일이_있는_경우에는_이메일_발송(booking_scheduler_with_mail_mock):
+    booking_scheduler, mail_mock = booking_scheduler_with_mail_mock
     schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER_WITH_MAIL)
 
     booking_scheduler.add_schedule(schedule)
 
-    assert mail_sender.send_mail_count == 1
+    assert mail_mock.send_mail_count == 1
 
 
 def test_현재날짜가_일요일인_경우_예약불가_예외처리():
