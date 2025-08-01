330ee61fc2f30af05eb6d06501b03f62820a560e
hyewonkim <hyewonds.kim@samsung.com>
2025-08-01 13:38:08 +0900

    Step11

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -16,6 +16,22 @@ UNDER_CAPACITY = 1
 CAPACITY_PER_HOUR = 3
 
 
+class SundayBookingScheduler(BookingScheduler):
+    def __init__(self, capacity_per_hour):
+        super().__init__(capacity_per_hour)
+
+    def get_now(self):
+        return datetime.strptime("2021/03/28 17:00", "%Y/%m/%d %H:%M")
+
+
+class MondayBookingScheduler(BookingScheduler):
+    def __init__(self, capacity_per_hour):
+        super().__init__(capacity_per_hour)
+
+    def get_now(self):
+        return datetime.strptime("2024/06/03 17:00", "%Y/%m/%d %H:%M")
+
+
 @pytest.fixture
 def booking_scheduler():
     return BookingScheduler(CAPACITY_PER_HOUR)
@@ -100,8 +116,17 @@ def test_이메일이_있는_경우에는_이메일_발송(booking_scheduler_wit
 
 
 def test_현재날짜가_일요일인_경우_예약불가_예외처리():
-    pass
+    booking_scheduler = SundayBookingScheduler(CAPACITY_PER_HOUR)
+    schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+
+    with pytest.raises(ValueError):
+        booking_scheduler.add_schedule(schedule)
 
 
 def test_현재날짜가_일요일이_아닌경우_예약가능():
-    pass
+    booking_scheduler = MondayBookingScheduler(CAPACITY_PER_HOUR)
+    schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+
+    booking_scheduler.add_schedule(schedule)
+
+    assert booking_scheduler.has_schedule(schedule)
