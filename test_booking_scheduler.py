4e9a3ec67108ea292a1267168c852bfabb97bb1a
hyewonkim <hyewonds.kim@samsung.com>
2025-08-01 12:46:49 +0900

    Step4

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -28,8 +28,13 @@ def test_예약은_정시에만_가능하다_정시인_경우_예약가능(booki
 
     assert booking_scheduler.has_schedule(schedule)
 
-def test_시간대별_인원제한이_있다_같은_시간대에_Capacity_초과할_경우_예외발생():
-    pass
+def test_시간대별_인원제한이_있다_같은_시간대에_Capacity_초과할_경우_예외발생(booking_scheduler):
+    schedule = Schedule(ON_THE_HOUR, CAPACITY_PER_HOUR, CUSTOMER)
+    booking_scheduler.add_schedule(schedule)
+
+    with pytest.raises(ValueError, match="Number of people is over restaurant capacity per hour"):
+        new_schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+        booking_scheduler.add_schedule(new_schedule)
 
 def test_시간대별_인원제한이_있다_같은_시간대가_다르면_Capacity_차있어도_스케쥴_추가_성공():
     pass
